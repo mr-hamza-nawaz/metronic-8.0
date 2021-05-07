f1315eaedbc1c5da1c41c22bc19ea2b7401a02c4
mr-hamza-nawaz <mr.hamza.nawaaz@gmail.com>
2021-05-07 13:21:25 +0500

    first commit

---
+++ html_bootstrap5/README.md
@@ -0,0 +1,12 @@
+# Metronic - Bootstrap 5 HTML, React, Angular 11, VueJS & Laravel Admin Dashboard Theme
+
+- Current version includes the HTML version with Demo 1 only, and other demos, Angular, React, VueJS  versions and other features will be added with Bootstrap 5 at the soonest.
+  We are working on this with the highest  priority. Stay tuned!
+
+- For a quick start please check [Online documentation page](//preview.keenthemes.com/metronic8/demo1/documentation/getting-started.html)
+
+- The offline documentation is available within the theme [Offline documentation page](//theme/dist/demo1/documentation/getting-started.html)
+
+- For any theme related questions please contact our [Theme Support](//keenthemes.com/support/)
+
+Happy coding with Metronic!
---
+++ html_bootstrap5/theme/tools/webpack/scripts.demo1.js
@@ -0,0 +1,24 @@
+// Keenthemes' plugins
+window.KTUtil = require('@/src/js/components/util.js');
+window.KTCookie = require('@/src/js/components/cookie.js');
+window.KTDialer = require('@/src/js/components/dialer.js');
+window.KTDrawer = require('@/src/js/components/drawer.js');
+window.KTEventHandler = require('@/src/js/components/event-handler.js');
+window.KTFeedback = require('@/src/js/components/feedback.js');
+window.KTImage = require('@/src/js/components/image-input.js');
+window.KTMenu = require('@/src/js/components/menu.js');
+window.KTPasswordMeter = require('@/src/js/components/password-meter.js');
+window.KTPlace = require('@/src/js/components/place.js');
+window.KTScroll = require('@/src/js/components/scroll.js');
+window.KTScrolltop = require('@/src/js/components/scrolltop.js');
+window.KTSearch = require('@/src/js/components/search.js');
+window.KTStepper = require('@/src/js/components/stepper.js');
+window.KTSticky = require('@/src/js/components/sticky.js');
+window.KTToggle = require('@/src/js/components/toggle.js');
+
+// Layout base js
+window.KTApp = require('@/src/js/layout/app.js');
+window.KTLayoutAside = require('@/src/js/layout/aside.js');
+window.KTLayoutExplore = require('@/src/js/layout/explore.js');
+window.KTLayoutSearch = require('@/src/js/layout/search.js');
+window.KTLayoutToolbar = require('@/src/js/layout/toolbar.js');
